cd434f3b149111416979aba992b755969f63398a
Delfina Quinteros <de.quinteros@um.edu.ar>
2021-08-26 17:07:37 -0300

    filtros

--- main/map/__init__.py
+++ main/map/__init__.py
@@ -1,9 +1,6 @@
 from .cliente_schema import Cliente_Schema
 from .empresa_schema import Empresa_Schema
 from .equipo_schema import Equipo_Schema
-<<<<<<< HEAD
 from .filtro_cliente import ClienteFiltros
-=======
 
->>>>>>> 0ffbba7845137d67670098c4a015d9dcbc70648c
 
